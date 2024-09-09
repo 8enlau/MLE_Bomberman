a983cb774dde141ed429296f19433415facd8eb4
Till Janke <till.janke@t-online.de>
2024-09-09 20:51:25 +0200

    rewards updated

--- neural_network/rewards.py
+++ neural_network/rewards.py
@@ -13,46 +13,46 @@ def reward(situationDictionary,action):
     after_action=position_after_step(situation,action)
     ### Impossible action
     if action_not_possible(situation,action,after_action):
-        return -2000
+        return -8
 
     ### Action leads to Dying
     if action_leads_to_suicide(situation,action,after_action): # For example blocking self in bomb
-        return -1000
+        return -4
     if action_leads_to_dying(situation,after_action):
-        return -1000
+        return -4
 
     ### Being close to a bomb
     # Walking where a bomb will explode soon:
     bombs=in_scope_of_bomb_after_action(situation,action,after_action)
     if bombs:
         if cannot_escape_after_action(situation,action,after_action,bombs):
-            return -1000
+            return -4
 
 
     ### Player can survive, try to maximise gain:
     possible_reward = 0
     if action=="BOMB":
         if bomb_will_kill_opponent(situation,after_action):
-            possible_reward +=1000
+            possible_reward +=4
         if bomb_might_kill_opponent(situation,after_action):
-            possible_reward += 25
+            possible_reward += 0.2
         if bomb_shortens_path_to_coin(situation,action,after_action):
-            possible_reward += 50
+            possible_reward += 0.25
         if bomb_will_destroy_crates(situation,after_action):
-            possible_reward += 15
+            possible_reward += 0.15
         return possible_reward
     if action != "WAIT":
-        possible_reward+=10
+        possible_reward+=0.1
     if action_leads_to_dying_opponent(situation,action,after_action): #For Example standing in the way
                                         # and therefore blocking opponent to stand in bomb.
-        possible_reward +=1000
+        possible_reward +=4
     if collecting_coin(situation,action,after_action):
-        possible_reward +=300
+        possible_reward +=2
     if walking_closer_to_reachable_coin(situation,after_action):
-        possible_reward +=200
+        possible_reward +=1
     if no_coin_reachable(situation,after_action):
         if closer_distance_to_coin(situation, after_action):
-            possible_reward +=100
+            possible_reward +=0.5
     return possible_reward
 
 
